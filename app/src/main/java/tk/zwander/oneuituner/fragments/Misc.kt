050b785bc4598cb5aa5efef9aab72b1bfd4c6725
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-10-14 16:52:15 -0400

    Use easier way to set EditTextPreference inputType

--- app/src/main/java/tk/zwander/oneuituner/fragments/Misc.kt
+++ app/src/main/java/tk/zwander/oneuituner/fragments/Misc.kt
@@ -2,6 +2,8 @@ package tk.zwander.oneuituner.fragments
 
 import android.os.Build
 import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import tk.zwander.oneuituner.R
 import tk.zwander.oneuituner.util.*
@@ -22,6 +24,18 @@ class Misc : Base() {
 
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
             setPreferenceEnabled(PrefManager.OLD_RECENTS, false)
+        }
+
+        findPreference<EditTextPreference>(PrefManager.FREEFORM_MAX_COUNT)?.apply {
+            setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+            }
+        }
+
+        findPreference<EditTextPreference>(PrefManager.FREEFORM_DESKTOP_MAX_COUNT)?.apply {
+            setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+            }
         }
     }
 }
